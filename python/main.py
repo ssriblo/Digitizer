3509310dc5e2c51ef631862264df13f90b1608d5
sskriblo <a.skriblo@gmail.com>
2021-08-12 22:03:36 +0300

    add Python and pulseview setup

--- python/main.py
+++ python/main.py
@@ -1,26 +1,45 @@
 import serial
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = 'COM11'
-print(ser)
-ser.open()
+import time
+import sys
+from cobs import cobs
 
-# while True:
-#     bytesToRead = ser.inWaiting()
-#     if bytesToRead > 0 :
-#         array = ser.read(bytesToRead)
-#         print(f'bytesToRead {bytesToRead} \n')
-#         for byte in array:
-#             print(byte),
-#             pass
+def main(arg):
+    ser = serial.Serial()
+    ser.baudrate = 115200*4
+    ser.port = 'COM12'
+    ser.set_buffer_size(rx_size = 12800, tx_size = 12800)
+    print(ser)
+    ser.open()
 
-while True:
-    arr = ser.read(10)
-    if arr != None:
-        print(f'LEN={len(arr)}')
-        bstr = bytearray(b'')
-        for byte in arr:
-            bstr.append(byte)
-        print(bstr)
-
-
+    buf = bytearray()
+    count = 0 #-1 # if count<0 then non-stop, if count=0 stop after NUMBER reading
+    NUMBER = 6
+    print("***********************************************************************")
+    while True:
+        chunk = ser.read(66)
+        if chunk != None:
+            if(count >=0): 
+                count = count + 1
+            print(f'************ COUNT={count}')
+            if(count > 5):
+                return
+            buf.extend(chunk)
+            bytes_to_drop = handle_buf(buf)
+            del buf[0:bytes_to_drop]  
+        
+def handle_buf(buf):
+    byte_proc = 0
+    zero_index = buf.index(0)
+    print(f'zero index={zero_index}')
+    print(f'handle_buff(): \n{buf.hex()}')
+    if(len(buf) > zero_index + 65):
+        cobs_buff = buf[zero_index + 1:zero_index+66]
+        print(f'COBS BUFF: \n{cobs_buff.hex()}')
+        decoded = cobs.decode(cobs_buff)
+        print(f'DECODED: \n{decoded.hex()}')
+        return zero_index + 66
+    else:
+        return 0
+    
+if __name__ == '__main__':
+    main(sys.argv)    
