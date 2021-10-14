9ef286aaa220a53bfeb535755f7eac01ec251a9a
sskriblo <a.skriblo@gmail.com>
2021-10-14 12:49:54 +0300

    add test print only

--- python/main.py
+++ python/main.py
@@ -3,11 +3,26 @@ import time
 import sys
 from cobs import cobs
 
+PRINTLOG = 0
+
+def printLog(a):
+    if( PRINTLOG > 0):
+        print(a)
+
 def main(arg):
+    global PRINTLOG
+    if(len(arg) > 1):
+        # arg[1] printLog flag
+        print(">>>-1")
+        if(arg[1] == "1"):
+            PRINTLOG = 1
+            print(">>>-2")
+            print(PRINTLOG)
+
     ser = serial.Serial()
-    ser.baudrate = 115200*4
-    ser.port = 'COM12'
-    ser.set_buffer_size(rx_size = 12800, tx_size = 12800)
+    ser.baudrate = 115200*8
+    ser.port = "/dev/ttyUSB0" #'COM12'
+#    ser.set_buffer_size(rx_size = 12800, tx_size = 12800)
     print(ser)
     ser.open()
 
@@ -20,7 +35,7 @@ def main(arg):
         if chunk != None:
             if(count >=0): 
                 count = count + 1
-            print(f'************ COUNT={count}')
+            printLog(f'************ COUNT={count}')
             if(count > 5):
                 return
             buf.extend(chunk)
@@ -30,13 +45,13 @@ def main(arg):
 def handle_buf(buf):
     byte_proc = 0
     zero_index = buf.index(0)
-    print(f'zero index={zero_index}')
-    print(f'handle_buff(): \n{buf.hex()}')
+    printLog(f'zero index={zero_index}')
+    printLog(f'handle_buff(): \n{buf.hex()}')
     if(len(buf) > zero_index + 65):
         cobs_buff = buf[zero_index + 1:zero_index+66]
-        print(f'COBS BUFF: \n{cobs_buff.hex()}')
+        printLog(f'COBS BUFF: \n{cobs_buff.hex()}')
         decoded = cobs.decode(cobs_buff)
-        print(f'DECODED: \n{decoded.hex()}')
+        printLog(f'DECODED: \n{decoded.hex()}')
         return zero_index + 66
     else:
         return 0
